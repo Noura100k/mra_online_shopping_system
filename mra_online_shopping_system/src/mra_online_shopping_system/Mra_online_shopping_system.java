d9bc4481afb33b906330827dfc42963b055bab77
Noura almusaynid <70031377+Noura100k@users.noreply.github.com>
2023-02-14 21:00:40 +0300

    first

---
+++ mra_online_shopping_system/src/mra_online_shopping_system/Mra_online_shopping_system.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mra_online_shopping_system;
+
+/**
+ *
+ * @author msi9
+ */
+public class Mra_online_shopping_system {
+public int price=0;
+     public void price()
+     {
+       
+       price=price+80;
+     
+     }
+       public void price1()
+     {
+       
+       price=price+80;
+     
+     }
+         public void price2()
+     {
+       
+       price=price+80;
+     
+     }
+         
+       public void print()  
+       {
+          
+       System.out.print(price);
+       
+       }
+     
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+           
+    }
+    
+}
